461d5c5309cbdeb3be2b7bf8cc8194812a26c00f
Matt Willhite <matthew.willhite@gmail.com>
2010-07-19 20:49:37 -0700

    Initial Commit

---
+++ application.rb
@@ -0,0 +1,18 @@
+%w( rubygems sinatra haml sass gravatar-ultimate ).each do |dependency|
+	require dependency
+end
+
+helpers do
+end
+
+# Styles
+get "/stylesheets/styles.css" do
+  headers 'Content-Type' => 'text/css; :charset=utf-8'
+	response['Expires'] = (Time.now + 60*60*24*356*3).httpdate
+  sass :styles
+end
+
+# GET
+get "/" do
+  haml :resume
+end
